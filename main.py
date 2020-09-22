773de26e816bbf1bbd9d34cd13d7632b92d2d896
Shaun-Jonker <shaun.c.jonker@gmail.com>
2020-09-22 21:44:36 +0200

    Added CPU Web Data Object.

--- main.py
+++ main.py
@@ -38,17 +38,22 @@ writer.append('Graphics Cards', wootware_graphics_cards)
 writer.append('Graphics Cards', foxytech_graphics_cards)
 
 
+# Monitor Web Data
+evetech_Monitors = evetech.eve_search("https://www.evetech.co.za/PC-Components/pc-monitors-89.aspx", "Monitors")
+wootware_Monitors = wootware.woot_search('https://www.wootware.co.za/computer-hardware/lcds-monitors/shopby/in_stock_with_wootware', 'Monitors')
+foxytech_Monitors = foxytech.foxy_search('https://www.foxytech.co.za/product-category/peripherals/monitors/?orderby=by_stock', 'Monitors')
+
+
 # Write Monitor data to File
-evetech_Monitors = evetech.eve_search("https://www.evetech.co.za/PC-Components/pc-monitors-89.aspx", "Graphics Cards")
-wootware_Monitors = wootware.woot_search('https://www.wootware.co.za/computer-hardware/lcds-monitors/shopby/in_stock_with_wootware', 'Graphics Cards')
-foxytech_Monitors = foxytech.foxy_search('https://www.foxytech.co.za/product-category/peripherals/monitors/?orderby=by_stock', 'Graphics Cards')
-
-
-# Write Graphics Cards data to File
 writer.write_to_file("Monitors", evetech_Monitors)
 writer.append('Monitors', wootware_Monitors)
 writer.append('Monitors', foxytech_Monitors)
 
+
+# CPU Web Data
+evetech_cpu = evetech.eve_search('https://www.evetech.co.za/components/buy-cpu-processors-online-164.aspx', "CPU")
+wootware_cpu = wootware.woot_search('https://www.wootware.co.za/computer-hardware/cpus-processors/shopby/in_stock_with_wootware', 'CPU')
+foxytech_cpu = foxytech.foxy_search("https://www.foxytech.co.za/product-category/hardware/computer-components/cpu-processors/?orderby=by_stock", "CPU")
 
 print('\nCompleted you can now Close this window')
 print('and check the folder for updated price lists')

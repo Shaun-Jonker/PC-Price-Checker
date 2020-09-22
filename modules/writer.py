18bc5373712a9c32a99c2dd9b7243be5c681c156
Shaun-Jonker <shaun.c.jonker@gmail.com>
2020-09-22 22:14:51 +0200

    Update writer.py

--- modules/writer.py
+++ modules/writer.py
@@ -5,7 +5,7 @@ class Writer:
 
     def write_to_file(self, file, product_list):
 
-        with open(f'../Price Lists/{file}.csv', "w", newline='', encoding="utf-8") as the_file:
+        with open(f'Price Lists/{file}.csv', "w", newline='', encoding="utf-8") as the_file:
             csv.register_dialect("custom", delimiter=",")
             writer = csv.writer(the_file, dialect="custom")
             for item in product_list:
@@ -14,7 +14,7 @@ class Writer:
 
     def append (self, file, product_list):
 
-        with open(f'../Price Lists/{file}.csv', "a+", newline='', encoding="utf-8") as the_file:
+        with open(f'Price Lists/{file}.csv', "a+", newline='', encoding="utf-8") as the_file:
             csv.register_dialect("custom", delimiter=",")
             writer = csv.writer(the_file, dialect="custom")
             for item in product_list:
